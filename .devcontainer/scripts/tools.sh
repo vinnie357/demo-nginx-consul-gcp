32caf50ceb27378e0cbc975fa3f08e468d5b10d0
Vinnie Mazza <vinnie.mazza@gmail.com>
2020-08-07 20:18:36 +0000

    add less to dev container for diffs

--- .devcontainer/scripts/tools.sh
+++ .devcontainer/scripts/tools.sh
@@ -3,7 +3,7 @@
 echo "cwd: $(pwd)"
 echo "---getting tools---"
 sudo apt-get update
-sudo apt-get install -y jq
+sudo apt-get install -y jq less
 . .devcontainer/scripts/gcloud.sh
 . .devcontainer/scripts/terraform.sh
 echo "---tools done---"
